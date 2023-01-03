2f346fba8b0023881aeedd90ab5b88624bf8cbb2
Mustapha7018 <adamsmustapha419@gmail.com>
2023-01-03 22:53:04 +0000

    nav changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,23 @@
+//creating hamburger toggle functionality
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    }
+
+    else if (visibility === 'true') {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+
+    }
+});
+
+// Marquee section
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector(".marquee-content");
